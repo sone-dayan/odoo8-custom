19ce47be2373b07977cfeb8ce3306051f4db7bc6
SoneDayan <sonedayan@gmail.com>
2025-04-04 16:14:44 +0100

    editing web digital sign

--- addons/web_digital_sign/__openerp__.py
+++ addons/web_digital_sign/__openerp__.py
@@ -31,30 +31,20 @@
     'description': '''
      This module provides the functionality to store digital signature
      for a record.
-        -> This  module is helpfull to make your business process a little
+        -> This  module is helpful to make your business process a little
            bit more faster & makes it more user friendly by providing you
            digital signature functionality on your documents.
         -> It is touch screen enable so user can add signature with touch
            devices.
-        -> Digital signature can be very usefull for documents such as
-           sale orders, purchase orders, invoices, payslips, procurement
-           receipts, etc.
-        The example can be seen into the User's form view where we have
-        added a test field under signature.
     ''',
     'summary': '''
      This module provides the functionality to store digital signature
      for a record.
-        -> This  module is helpfull to make your business process a little
+        -> This  module is helpful to make your business process a little
            bit more faster & makes it more user friendly by providing you
            digital signature functionality on your documents.
         -> It is touch screen enable so user can add signature with touch
            devices.
-        -> Digital signature can be very usefull for documents such as
-           sale orders, purchase orders, invoices, payslips, procurement
-           receipts, etc.
-        The example can be seen into the User's form view where we have
-        added a test field under signature.
     ''',
     'images': ['static/description/Digital_Signature.jpg'],
     'data': [
