8a44fa8c818de4e8da35f22d965c2896f90f9cff
SoneDayan <sonedayan@gmail.com>
2025-04-10 14:16:15 +0100

    testing save button generate plan

--- addons/hr_payslip_advance/models/hr_payslip_advance.py
+++ addons/hr_payslip_advance/models/hr_payslip_advance.py
@@ -66,27 +66,28 @@ class HrPayslipAdvance(models.Model):
         if context is None:
             context = {}
 
-        # Generate sequence if not set
-        if vals.get('number', '/') == '/':
-            vals['number'] = self.pool.get('ir.sequence').get(cr, uid, 'hr.payslip.advance')
-
         record_id = super(HrPayslipAdvance, self).create(cr, uid, vals, context=context)
         record = self.browse(cr, uid, record_id, context=context)
-        if vals.get('generate_lines') and vals.get('reimburse_monthly'):
-            self._generate_repayment_lines(cr, uid, [record.id], context=context)
+
+        # Only generate if the record has all required fields set
+        if record.generate_lines and record.reimburse_monthly and \
+        record.repayment_start_date and record.monthly_amount > 0:
+            self._generate_repayment_lines(cr, uid, [record_id], context=context)
+
         return record_id
 
     def write(self, cr, uid, ids, vals, context=None):
-        if hasattr(self, '_generating_repayment'):
-            return super(HrPayslipAdvance, self).write(cr, uid, ids, vals, context=context)
-        self._generating_repayment = True
-        try:
-            # existing write logic
-            res = super(HrPayslipAdvance, self).write(cr, uid, ids, vals, context=context)
-            self._generate_repayment_lines(cr, uid, ids, context=context)
-            return res
-        finally:
-            del self._generating_repayment
+        if context is None:
+            context = {}
+
+        res = super(HrPayslipAdvance, self).write(cr, uid, ids, vals, context=context)
+
+        for record in self.browse(cr, uid, ids, context=context):
+            if record.generate_lines and record.reimburse_monthly and \
+            record.repayment_start_date and record.monthly_amount > 0:
+                self._generate_repayment_lines(cr, uid, [record.id], context=context)
+
+        return res
 
     def _generate_repayment_lines(self, cr, uid, ids, context=None):
         if context is None:
